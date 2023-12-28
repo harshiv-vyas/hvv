1a0254bab6fd27136aa9c76f30e84527932ee3b7
harshiv-vyas <hrvyas.2006@gmail.com>
2023-12-28 11:38:56 +0530

    i am your brother

---
+++ hv.c
@@ -0,0 +1,94 @@
+//que 1
+/*#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter your number:");
+    scanf("%d",&a);
+    (a%2==0)?printf("number is even"):printf("number is odd");
+    return 0;
+}*/
+
+
+
+//que 2
+/*#include<stdio.h>
+
+int main()
+{
+    int month;
+    printf("enter month:");
+    scanf("%d",&month);
+    switch(month){
+        case 1:{
+            printf("jan");
+            break;
+        }
+        case 2:{
+            printf("feb");
+            break;
+        }
+        case 3:{
+            printf("march");
+            break;
+        }
+        case 4:{
+            printf("april");
+            break;
+        }
+        case 5:{
+            printf("may");
+            break;
+        }
+        case 6:{
+            printf("jun");
+            break;
+        }
+        case 7:{
+            printf("juliy");
+            break;
+        }
+        case 8:{
+            printf("aug");
+            break;
+        }
+        case 9:{
+            printf("sept");
+            break;
+        }
+        case 10:{
+            printf("oct");
+            break;
+        }
+        case 11:{
+            printf("nov");
+            break;
+        }
+        case 12:{
+            printf("dec");
+            break;
+        }
+        default:{
+            printf("invalid number");
+            
+        }
+    }
+    return 0;
+}*/
+
+
+
+//que 3
+
+/*#include<stdio.h>
+
+int main()
+{
+    int year;
+    printf("enter year");
+    scanf("%d",&year);
+    (year%4==0)?printf("year is leep"):printf("year is not leep");
+    return 0;
+    
+}*/
